9da0248f099b0dfef876ceda969048ce47d4e1b8
Lovsog <840018730@qq.com>
2022-11-01 15:14:18 +0800

    FastBeeIM 极蜂IM

---
+++ src/main/java/com/fastbee/fastbeeim/FastBeeIMApplication.java
@@ -0,0 +1,13 @@
+package com.fastbee.fastbeeim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastBeeIMApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastBeeIMApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastbee/fastbeeim/FastBeeImApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastbee.fastbeeim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastBeeImApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
