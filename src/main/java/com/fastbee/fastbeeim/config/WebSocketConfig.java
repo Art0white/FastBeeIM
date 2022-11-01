bb631a8c379855bea858f3e906136f128bf9f20b
Lovsog <840018730@qq.com>
2022-11-01 15:59:17 +0800

    FastBeeIM 极蜜IM

---
+++ src/main/java/com/fastbee/fastbeeim/config/WebSocketConfig.java
@@ -0,0 +1,8 @@
+package com.fastbee.fastbeeim.config;
+
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
+}
