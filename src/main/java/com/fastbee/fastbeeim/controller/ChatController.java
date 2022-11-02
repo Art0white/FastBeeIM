831a213f5e1c80523571babda7a9a840c5388923
Lovsog <840018730@qq.com>
2022-11-02 16:07:04 +0800

    FastBeeIM 极蜜IM

---
+++ src/main/java/com/fastbee/fastbeeim/controller/ChatController.java
@@ -0,0 +1,30 @@
+package com.fastbee.fastbeeim.controller;
+
+import com.fastbee.fastbeeim.pojo.User;
+import com.fastbee.fastbeeim.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class ChatController {
+    @Autowired
+    private IUserService userService;
+
+    @GetMapping("/getOneUser")
+    public User getUserByUserName(String username) {
+        System.out.println("执行了这里, " + username);
+        System.out.println(userService.getUserByUserName(username).toString());
+        return userService.getUserByUserName(username);
+    }
+
+    @GetMapping("/getUserList")
+    public List<User> getAllUsers(String keywords) {
+        return userService.getAllUsers(keywords);
+    }
+
+}
