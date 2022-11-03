bdb38bc213363fa04024df07fa16330cd77b07ba
Lovsog <840018730@qq.com>
2022-11-03 18:45:31 +0800

    FastBeeIM 极蜜IM 示例

--- src/main/resources/static/js/chatPage.js
+++ src/main/resources/static/js/chatPage.js
@@ -1,6 +1,12 @@
+function getQueryString(name) {
+    const url_string = window.location.href; // window.location.href
+    const url = new URL(url_string);
+    return url.searchParams.get(name);
+}
+
 var webSocket;
-var username = sessionStorage.getItem("username");
-alert("我的，名字叫    "+username);
+var username = getQueryString("username");
+alert("我的，名字叫    " + username);
 var userTo = document.getElementById('userTo').value;
 if ("WebSocket" in window)
 {
@@ -18,47 +24,15 @@ if ("WebSocket" in window)
     webSocket.onmessage = function (evt)
     {
         var received_msg = evt.data;
-        console.log("数据已接收:" +received_msg);
-        var obj = JSON.parse(received_msg);
+        console.log("数据已接收:" + received_msg);
+        var obj = JSON.parse(received_msg).message;
         console.log("可以解析成json:"+obj.messageType);
-        //1代表上线 2代表下线 3代表在线名单 4代表普通消息
+        /**
+         * 1: 文本消息
+         */
         if(obj.messageType==1){
-            //把名称放入到selection当中供选择
-            var onlineName = obj.username;
-            var li = "<li class='setLi'>"+onlineName+"</li><hr>";
-            $("#onLineUser").append(li);
-            setMessageInnerHTML(onlineName+"上线了");
-        }
-        else if(obj.messageType==2){
-            $("#onLineUser").empty();
-            var onlineName = obj.onlineUsers;
-            var offlineName = obj.username;
-            var li = "<li class='setLi'>"+"--所有--"+"</li><hr>";
-            for(var i=0;i<onlineName.length;i++){
-                if(!(onlineName[i]==username)){
-                    li+="<li class='setLi'>"+onlineName[i]+"</li><hr>"
-                }
-            }
-            $("#onLineUser").append(li);
-
-            setMessageInnerHTML(offlineName+"下线了");
-        }
-        else if(obj.messageType==3){
-            var onlineName = obj.onlineUsers;
-            var li = null;
-            for(var i=0;i<onlineName.length;i++){
-                if(!(onlineName[i]==username)){
-                    li+="<li class='setLi'>"+onlineName[i]+"</li><hr>"
-                }
-            }
-            $("#onLineUser").append(li);
-            console.log("获取了在线的名单"+onlineName.toString());
-        }
-        else{
-            //var divMsg = "<tr><td style='text-align:left'>"+obj.fromusername+"对"+obj.tousername+"说：<br>"+obj.textMessage+"</td></tr>"
-            var divMsg = "<tr><td style='text-align:left'>"+obj.fromusername+"：<br>"+obj.textMessage+"</td></tr>"
+            var divMsg = "<tr><td style='text-align:left'>"+obj.from+"：<br>"+obj.content+"</td></tr>"
             $("#message").append(divMsg);
-            //setMessageInnerHTML();
         }
     };
 
@@ -86,13 +60,15 @@ function closeWebSocket() {
 
 function send(){
     var userTo = document.getElementById('userTo').value;
-    var userFrom = sessionStorage.getItem("username");
+    var userFrom = username
     var divMsg = "<tr><td style='text-align:right'>"+userFrom+":<br> "+$("#text").val()+"</td></tr>"
     $("#message").append(divMsg);
     var message = {
-        "message":document.getElementById('text').value,
+        "content":document.getElementById('text').value,
         "from":userFrom,
-        "to":userTo
+        "to":userTo,
+        "messageType":1,
+        "sendMessageType":1,
     };
     webSocket.send(JSON.stringify(message));
     $("#text").val("");
