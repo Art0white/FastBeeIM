8d766b5f9143e0e14464a2e822df5ee025f6c697
Lovsog <840018730@qq.com>
2022-11-04 17:39:52 +0800

    ⌨refactor: 修改

--- src/main/java/com/fastbee/fastbeeim/controller/WebSocketController.java
+++ src/main/java/com/fastbee/fastbeeim/controller/WebSocketController.java
@@ -4,7 +4,6 @@ import com.fastbee.fastbeeim.common.FSIMWebSocketServer;
 import com.fastbee.fastbeeim.utils.RespBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
