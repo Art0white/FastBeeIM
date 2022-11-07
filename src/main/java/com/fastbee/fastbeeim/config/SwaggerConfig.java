fef6ad1841dc2016bbd7c0518f5b7e3d1ea1060e
Lovsog <840018730@qq.com>
2022-11-07 15:28:50 +0800

    ⌨feat: 集成Swagger接口文档

--- src/main/java/com/fastbee/fastbeeim/config/SwaggerConfig.java
+++ src/main/java/com/fastbee/fastbeeim/config/SwaggerConfig.java
@@ -6,12 +6,8 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
