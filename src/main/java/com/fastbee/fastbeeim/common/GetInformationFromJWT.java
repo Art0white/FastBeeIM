1e6559efac15309f9c3f78fa4914e8bc199072b0
Lovsog <840018730@qq.com>
2022-11-07 15:24:43 +0800

    ⌨feat: 应用类, 实现不同应用之间的隔离

--- src/main/java/com/fastbee/fastbeeim/common/GetInformationFromJWT.java
+++ src/main/java/com/fastbee/fastbeeim/common/GetInformationFromJWT.java
@@ -1,7 +1,7 @@
 package com.fastbee.fastbeeim.common;
 
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.fastbee.fastbeeim.utils.JWTUtils;
+import com.fastbee.fastbeeim.utils.FBApplicationsUtils;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,21 +9,15 @@ import javax.servlet.http.HttpServletRequest;
 @Component
 public class GetInformationFromJWT {
 
-    public int getUserIdByJWT(HttpServletRequest request){
-        String token = request.getHeader("token");
-        DecodedJWT verify = JWTUtils.verify(token);
-        return Integer.parseInt(verify.getClaim("userId").asString());
-    }
-
-    public int getRoleIdByJWT(HttpServletRequest request){
-        String token = request.getHeader("token");
-        DecodedJWT verify = JWTUtils.verify(token);
-        return Integer.parseInt(verify.getClaim("roleId").asString());
-    }
-
-    public String getUsernameByJWT(HttpServletRequest request){
-        String token = request.getHeader("token");
-        DecodedJWT verify = JWTUtils.verify(token);
-        return verify.getClaim("username").asString();
+    public Integer getAppIdByAppKey(HttpServletRequest request){
+        String appKey = request.getHeader("appKey");
+        String appSecret = request.getHeader("appSecret");
+        DecodedJWT verify = null;
+        try {
+            verify = FBApplicationsUtils.verify(appKey, appSecret);
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+        return Integer.parseInt(verify.getClaim("appId").asString());
     }
 }
