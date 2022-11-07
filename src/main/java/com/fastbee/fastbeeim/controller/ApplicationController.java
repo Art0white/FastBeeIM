b6fcca3a34fb4e1672be7998c4d352325d9ae53a
Lovsog <840018730@qq.com>
2022-11-07 15:25:23 +0800

    ⌨feat: 应用类, 实现不同应用之间的隔离

---
+++ src/main/java/com/fastbee/fastbeeim/controller/ApplicationController.java
@@ -0,0 +1,31 @@
+package com.fastbee.fastbeeim.controller;
+import com.fastbee.fastbeeim.pojo.FBApplication;
+import com.fastbee.fastbeeim.service.impl.FBApplicationServiceImpl;
+import com.fastbee.fastbeeim.utils.RespBean;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * 应用相关API
+ * @author Lovsog
+ */
+
+@Api(tags = "应用相关API")
+@RestController
+@RequestMapping("/api/app")
+public class ApplicationController {
+
+    @Resource
+    private FBApplicationServiceImpl fbApplicationService;
+
+    @PostMapping("/createApp")
+    public RespBean creatApplication(String appName) {
+        String mySecret = System.currentTimeMillis() + "";     //当前时间戳
+        FBApplication fbApplication = fbApplicationService.createApp(appName, mySecret);
+        return new RespBean(200, "创建应用成功", fbApplication);
+    }
+}
