82b83d0f43c5919e7aec5176a4fb2b9d6c70bcde
Lovsog <840018730@qq.com>
2022-11-07 15:27:51 +0800

    ⌨feat: 去掉User登录在服务端实现的相关方法,之后在客户端SDK来实现

--- src/main/java/com/fastbee/fastbeeim/controller/LoginController.java
+++ src/main/java/com/fastbee/fastbeeim/controller/LoginController.java
@@ -15,35 +15,41 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
-@Api(tags = "用户登录相关API")
-@RestController
-@Validated
-@CrossOrigin
-public class LoginController {
-    @Resource
-    private UserServiceImpl userService;
 
-    @PostMapping("/login")
-    public RespBean login(LoginDTO loginDTO, HttpServletResponse response) {
-//        String salt = userService.getUserByUserName(loginDTO.getUsername());
-//        String password = new SimpleHash("MD5", loginDTO.getPassword(), salt, 1).toHex();
-        String password = loginDTO.getPassword();
-        String realPassword = userService.getPasswordByUserName(loginDTO.getUsername());
-        if(ObjectUtils.isEmpty(realPassword)){
-            return new RespBean(400, "用户名错误",0);
-        } else if (!realPassword.equals(password)) {
-            return new RespBean(400, "密码错误", 0);
-        } else {
-            User user = userService.getUserByUserName(loginDTO.getUsername());
-            Map<String, String> tokenMap = new HashMap<>();
-            tokenMap.put("token", userService.getToken(user));
-            return new RespBean(200,"success", tokenMap);
-        }
-    }
+/**
+ * 服务端不需要，之后移到客户端SDK
+ */
 
-    @PostMapping("/mytest")
-    public RespBean test(HttpServletRequest request) {
-        String username = userService.getUsernameByJWT(request);
-        return new RespBean(200,"success", username);
-    }
-}
+
+//@Api(tags = "用户登录相关API")
+//@RestController
+//@Validated
+//@CrossOrigin
+//public class LoginController {
+//    @Resource
+//    private UserServiceImpl userService;
+//
+//    @PostMapping("/login")
+//    public RespBean login(LoginDTO loginDTO, HttpServletResponse response) {
+////        String salt = userService.getUserByUserName(loginDTO.getUsername());
+////        String password = new SimpleHash("MD5", loginDTO.getPassword(), salt, 1).toHex();
+//        String password = loginDTO.getPassword();
+//        String realPassword = userService.getPasswordByUserName(loginDTO.getUsername());
+//        if(ObjectUtils.isEmpty(realPassword)){
+//            return new RespBean(400, "用户名错误",0);
+//        } else if (!realPassword.equals(password)) {
+//            return new RespBean(400, "密码错误", 0);
+//        } else {
+//            User user = userService.getUserByUserName(loginDTO.getUsername());
+//            Map<String, String> tokenMap = new HashMap<>();
+//            tokenMap.put("token", userService.getToken(user));
+//            return new RespBean(200,"success", tokenMap);
+//        }
+//    }
+//
+//    @PostMapping("/mytest")
+//    public RespBean test(HttpServletRequest request) {
+//        String username = userService.getUsernameByJWT(request);
+//        return new RespBean(200,"success", username);
+//    }
+//}
