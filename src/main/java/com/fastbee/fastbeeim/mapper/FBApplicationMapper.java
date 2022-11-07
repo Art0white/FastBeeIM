0795aec3e5ec0efba4f89bfd494598289564bcb3
Lovsog <840018730@qq.com>
2022-11-07 15:24:25 +0800

    ⌨feat: 应用类, 实现不同应用之间的隔离

--- src/main/java/com/fastbee/fastbeeim/mapper/FBApplicationMapper.java
+++ src/main/java/com/fastbee/fastbeeim/mapper/FBApplicationMapper.java
@@ -2,6 +2,7 @@ package com.fastbee.fastbeeim.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.fastbee.fastbeeim.pojo.FBApplication;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * FBApplicationMapper
@@ -9,4 +10,11 @@ import com.fastbee.fastbeeim.pojo.FBApplication;
  */
 
 public interface FBApplicationMapper extends BaseMapper<FBApplication> {
+    @Select("SELECT\n" +
+            "    AUTO_INCREMENT\n" +
+            "FROM\n" +
+            "    INFORMATION_SCHEMA.TABLES\n" +
+            "WHERE\n" +
+            "    TABLE_NAME = 'f_b_application'")
+    Integer getNextAutoIncrementOfApp();
 }
