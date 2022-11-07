0432fe28b1e558e36f80697c918817bc489d59bc
Lovsog <840018730@qq.com>
2022-11-07 15:24:53 +0800

    ⌨feat: 应用类, 实现不同应用之间的隔离

--- src/main/java/com/fastbee/fastbeeim/service/IFBApplicationService.java
+++ src/main/java/com/fastbee/fastbeeim/service/IFBApplicationService.java
@@ -1,7 +1,22 @@
 package com.fastbee.fastbeeim.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.fastbee.fastbeeim.dto.FBApplicationDTO;
 import com.fastbee.fastbeeim.pojo.FBApplication;
 
+import javax.servlet.http.HttpServletRequest;
+
 public interface IFBApplicationService extends IService<FBApplication> {
+
+    FBApplication getFBApplicationByAppId(Integer appId);
+
+    FBApplication getFBApplicationByAppName(String appName);
+
+    String getAppKey(FBApplication application);
+
+    String getAppSecret(FBApplication application);
+
+    FBApplication createApp(String appName, String secret);
+
+    Integer getAppIdByAppKey(HttpServletRequest request);
 }
