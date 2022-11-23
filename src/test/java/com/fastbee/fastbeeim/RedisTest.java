907a966db5a257471560b19944677e237ce6d3cf
Lovsog <840018730@qq.com>
2022-11-23 09:18:17 +0800

    🎈feat:添加Redis作缓存

---
+++ src/test/java/com/fastbee/fastbeeim/RedisTest.java
@@ -0,0 +1,15 @@
+package com.fastbee.fastbeeim;
+
+import org.junit.Test;
+
+/**
+ * @author Lovsog
+ * 2022/11/22
+ **/
+public class RedisTest {
+    @Test
+    public void RedisTest() throws Exception {
+
+    }
+}
+
